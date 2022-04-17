3b3af06b681c5cf29f735cd4e2285a6b0ba19d92
W. Guilherme <wguilhermecriativo@gmail.com>
2022-04-17 12:26:57 -0300

    feat: add product update (put)

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -19,3 +19,17 @@ exports.listAllProducts = async (req, res) => {
   const response = await db.query('SELECT * FROM products ORDER BY productname ASC');
   res.status(200).send(response.rows);
 }
+exports.findProductById = async (req, res) => {
+  const productId = parseInt(req.params.id)
+  const response = await db.query
+    ('SELECT * FROM products WHERE productid = $1', [productId]);
+  res.status(200).send(response.rows);
+}
+exports.updateProductById = async (req, res) => {
+  const productId = parseInt(req.params.id)
+  const { productname, quantity, price } = req.body
+
+  const response = await db.query('UPDATE products SET productname = $1, quantity = $2, price = $3 WHERE productId = $4', [productname, quantity, price, productId]);
+  // UPDATE products SET product_name = $1, quantity = $2, price = $3 WHERE productId = $4
+  res.status(200).send(response.rows);
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,5 +3,7 @@ const productController = require('../controllers/product.controller');
 
 router.post('/products', productController.createProduct);
 router.get('/products', productController.listAllProducts);
+router.get('/products/:id', productController.findProductById);
+router.put('/products/:id', productController.updateProductById);
 
 module.exports = router;
