cf2a69f7357f702fcfbed620715e3e57184f05bb
W. Guilherme <wguilhermecriativo@gmail.com>
2022-04-17 11:18:28 -0300

    fix: dotenv import, product routes + update product controller

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 
-const routes = require('./routes/index');
-
-// const productRoute = require('./routes/product.routes');
+const index = require('./routes/index');
+const productRoute = require('./routes/product.routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
-app.use(routes);
-// app.use('/api/', productRoute);
+app.use(index);
+app.use('/api/', productRoute);
 
 module.exports = app;
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,16 @@
-const db = require('../config/database')
-exports.createProduct = async(req,res)=>{
-  const {product_name, quantity, price}=req.body
-  const {rows}= await db.query
-}
+const db = require("../config/database");
+
+exports.createProduct = async (req, res) => {
+  const { product_name, quantity, price } = req.body;
+  const { rows } = await db.query(
+    "INSERT INTO products (product_name, quantity, price) VALUES ($1, $2, $3)",
+    [product_name, quantity, price]
+  );
+
+  res.status(201).send({
+    message: "Product added successfully!",
+    body: {
+      product: { product_name, quantity, price }
+    },
+  });
+};
